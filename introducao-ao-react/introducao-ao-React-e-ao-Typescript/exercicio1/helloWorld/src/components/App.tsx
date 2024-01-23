c7a96a0410c2e06107874016052a12f86bc044e3
PauloFonseca <pfonseca.arq@gmail.com>
2024-01-23 14:09:06 -0300

    introducao ao react

---
+++ introducao-ao-react/introducao-ao-React-e-ao-Typescript/exercicio1/helloWorld/src/components/App.tsx
@@ -0,0 +1,14 @@
+import ModuleDetails from './ModuleDetails';
+import Title from './Title';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ introducao-ao-react/introducao-ao-React-e-ao-Typescript/exercicio1/helloWorld/src/components/ModuleDetails.tsx
@@ -0,0 +1,12 @@
+function ModuleDetails() {
+  return (
+    <div>
+      <p>Fundamentos</p>
+      <p>Front-end</p>
+      <p>Back-end</p>
+      <p>Ciencias da Computacao</p>
+    </div>
+  );
+}
+
+export default ModuleDetails;
